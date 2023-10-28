2095beb8194b4018ef5d614256746ee4fa667d53
CHENLY-1 <129770311+CHENLY-1@users.noreply.github.com>
2023-10-29 02:02:40 +0800

    Update jd|tb.js

--- Rewrites/jd|tb.js
+++ Rewrites/jd|tb.js
@@ -4,6 +4,7 @@
 ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/CHENLY-1/Quantumult-X/main/Rewrites/jd%7Ctb.js
 
 ^http://.+/amdc/mobileDispatch url script-request-body https://raw.githubusercontent.com/CHENLY-1/Quantumult-X/main/Rewrites/jd%7Ctb.js
+^http://.+/amdc/mobileDispatch url script-response-body https://raw.githubusercontent.com/CHENLY-1/Quantumult-X/main/Rewrites/jd%7Ctb.js
 ^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail url script-response-body https://raw.githubusercontent.com/CHENLY-1/Quantumult-X/main/Rewrites/jd%7Ctb.js
 
 hostname = api.m.jd.com, trade-acs.m.taobao.com
