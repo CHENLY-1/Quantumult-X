5cdf53c456ea399efae92d52e8c0061ff507885c
CHENLY-1 <129770311+CHENLY-1@users.noreply.github.com>
2023-11-10 11:05:51 +0800

    更新 nicegram.js

---
+++ Scripts/break/nicegram.js
@@ -0,0 +1,21 @@
+// https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Nicegrampro.js
+/******************************
+脚本功能：Nicegram: Unlimited Text+解锁订阅
+软件版本：1.1.2
+脚本作者：彭于晏💞
+更新时间：2022-10-8
+*******************************
+
+[rewrite_local]
+
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Nicegram.js
+
+[mitm] 
+hostname=restore-access.indream.app
+
+*******************************/
+{
+  "data" : {
+    "premiumAccess" : true
+  }
+}
