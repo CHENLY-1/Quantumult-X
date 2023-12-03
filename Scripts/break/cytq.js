13b79181996de445f31aa77c46bd45f60b89ae60
CHENLY-1 <129770311+CHENLY-1@users.noreply.github.com>
2023-12-03 18:34:46 +0800

    更新 cytq.js

--- Scripts/break/cytq.js
+++ Scripts/break/cytq.js
@@ -16,23 +16,23 @@
 hostname = wrapper.cyapi.cn, biz.cyapi.cn
 ====================================
  */
-var caicoayun = {},
+var huihui = {},
     url = $request.url;
 if (url.includes("/v2/user")) {
     let obj = JSON.parse($response.body);
     obj.result.is_vip = !0,
         obj.result.svip_expired_at = 3742732800,
         obj.result.vip_type = "s",
-        caixiaoyun.body = JSON.stringify(obj)
+        huihui.body = JSON.stringify(obj)
 }
 if (/v1\/(satellite|nafp\/origin_images)/g.test(url)) {
-    caixiaoyun.headers = $request.headers;
-    caixiaoyun.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
+    huihui.headers = $request.headers;
+    huihui.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
 }
 if(url.includes('v1/activity')){
 	let body = $response.body
 	body = '{"status":"ok","activities":[{"items":[{}]}]}';
-	caiciaoyun.body = body;
+	huihui.body = body;
 }
 $done(huihui);
 
