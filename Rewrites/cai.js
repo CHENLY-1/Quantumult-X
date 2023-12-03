6a0c1779189bacf0273431e3ee541c09810fb8f7
CHENLY-1 <129770311+CHENLY-1@users.noreply.github.com>
2023-12-03 17:36:40 +0800

    更新 cai.js

--- Rewrites/cai.js
+++ Rewrites/cai.js
@@ -16,6 +16,13 @@ hostname = *.cyapi.cn
  */
 var caixiaoyun = {},
     url = $request.url;
+if (url.includes("/v2/user")) {
+    let obj = JSON.parse($response.body);
+    obj.result.is_vip = !0,
+        obj.result.svip_expired_at = 4092599349,
+        obj.result.vip_type = "s",
+        caixiaoyun.body = JSON.stringify(obj)
+}
 if (/v1\/(satellite|nafp\/origin_images)/g.test(url)) {
     caixiaoyun.headers = $request.headers;
     caixiaoyun.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
