fbb56a61504c959c8d0fc3fc63163020016da49c
CHENLY-1 <129770311+CHENLY-1@users.noreply.github.com>
2023-12-03 18:40:33 +0800

    更新 cytq.js

--- Scripts/break/cytq.js
+++ Scripts/break/cytq.js
@@ -1,6 +1,6 @@
 // https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
 /*
-彩云天气解锁S
+彩云天气解锁SVIP
 ====================================
 [rewrite_local]
 # 普通版广告
@@ -13,28 +13,28 @@
 ^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/CHENLY-1/Quantumult-X/main/Rewrites/cai.js
 
 [mitm]
-hostname = wrapper.cyapi.cn, biz.cyapi.cn
+hostname = biz.cyapi.cn, wrapper.cyapi.cn
 ====================================
  */
-var huihui = {},
+var caixiaoyun = {},
     url = $request.url;
 if (url.includes("/v2/user")) {
     let obj = JSON.parse($response.body);
     obj.result.is_vip = !0,
         obj.result.svip_expired_at = 3742732800,
         obj.result.vip_type = "s",
-        huihui.body = JSON.stringify(obj)
+        caixiaoyun.body = JSON.stringify(obj)
 }
 if (/v1\/(satellite|nafp\/origin_images)/g.test(url)) {
-    huihui.headers = $request.headers;
-    huihui.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
+    caixiaoyun.headers = $request.headers;
+    caixiaoyun.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
 }
 if(url.includes('v1/activity')){
 	let body = $response.body
 	body = '{"status":"ok","activities":[{"items":[{}]}]}';
-	huihui.body = body;
+	caixiaoyun.body = body;
 }
-$done(huihui);
+$done(caixiaoyun);
 
 
 //let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY"; 
