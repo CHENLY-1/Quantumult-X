e1ef1797aa30ba13a85c2bb5a688a9a1d7a39c7c
CHENLY-1 <129770311+CHENLY-1@users.noreply.github.com>
2023-12-03 17:50:03 +0800

    更新 cytq.js

--- Scripts/break/cytq.js
+++ Scripts/break/cytq.js
@@ -1,38 +1,37 @@
-// https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
-// 2023-07-11 08:35
+//https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
+/*
+彩云天气解锁SVIP
+====================================
+[rewrite_local]
+# 去除赏叶赏花
+^https:\/\/wrapper\.cyapi\.cn\/v1\/activity\?app_name=weather url script-response-body https://raw.githubusercontent.com/CHENLY-1/Quantumult-X/main/Rewrites/cai.js
+# 解锁vip
+^https:\/\/biz\.cyapi\.cn\/v2\/user url script-response-body https://raw.githubusercontent.com/CHENLY-1/Quantumult-X/main/Rewrites/cai.js
+# 卫星云图 48小时预报
+^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/CHENLY-1/Quantumult-X/main/Rewrites/cai.js
 
-const url = $request.url;
-if (!$response.body) $done({});
-let obj = JSON.parse($response.body);
-
-if (url.includes("/v1/vip_info")) {
-  if (obj.vip) {
-    obj.vip.expires_time = "4030000000";
-  }
-  if (obj.svip) {
-    obj.svip.expires_time = "4030000000";
-  }
-} else if (url.includes("/v2/user")) {
-  if (obj.result) {
-    obj.result.svip_given = 730;
-    obj.result.is_phone_verified = true;
-    obj.result.is_xy_vip = true;
-    obj.result.vip_expired_at = 4030000000.16;
-    obj.result.is_vip = true;
-    obj.result.xy_svip_expire = 4030000000.16;
-    if (obj.result.wt) {
-      if (obj.result.wt.vip) {
-        obj.result.wt.vip.enabled = true;
-        obj.result.wt.vip.expired_at = 4030000000.16;
-        obj.result.wt.vip.svip_expired_at = 4030000000.16;
-      }
-      obj.result.wt.svip_given = 730;
-    }
-    obj.result.is_primary = true;
-    obj.result.xy_vip_expire = 4030000000.16;
-    obj.result.svip_expired_at = 4030000000.16;
-    obj.result.vip_type = "s";
-  }
+[mitm]
+hostname = *.cyapi.cn
+====================================
+ */
+var caixiaoyun = {},
+    url = $request.url;
+if (url.includes("/v2/user")) {
+    let obj = JSON.parse($response.body);
+    obj.result.is_vip = !0,
+        obj.result.svip_expired_at = 3742732800,
+        obj.result.vip_type = "s",
+        caixiaoyun.body = JSON.stringify(obj)
 }
+if (/v1\/(satellite|nafp\/origin_images)/g.test(url)) {
+    caixiaoyun.headers = $request.headers;
+    caixiaoyun.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
+}
+if(url.includes('v1/activity')){
+	let body = $response.body
+	body = '{"status":"ok","activities":[{"items":[{}]}]}';
+	caixiaoyun.body = body;
+}
+$done(caixiaoyun);
 
-$done({ body: JSON.stringify(obj) });
+//let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY"; 
