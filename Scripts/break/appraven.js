06939faf27ef9df8a66e7812c8950c6d0eea7778
CHENLY-1 <129770311+CHENLY-1@users.noreply.github.com>
2023-12-22 23:33:29 +0800

    更新 appraven.js

--- Scripts/break/appraven.js
+++ Scripts/break/appraven.js
@@ -1,9 +1,9 @@
 //https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/AnyDown.js
-
+/*************************************
 ^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/CHENLY-1/Quantumult-X/main/Scripts/break/appraven.jsAnyDown_crack.js
 hostname = buy.itunes.apple.com
 
-*/
+**************************************/
 var chxm1023 = JSON.parse($response.body);
 
 chxm1023 = {
