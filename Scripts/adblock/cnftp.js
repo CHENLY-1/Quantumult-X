a6f2f497c111ed542e3b1f34739a15715c1230f1
CHENLY-1 <129770311+CHENLY-1@users.noreply.github.com>
2024-01-14 18:46:11 +0800

    Update cnftp.js

--- Scripts/adblock/cnftp.js
+++ Scripts/adblock/cnftp.js
@@ -209,11 +209,186 @@ if (isIQY) {
       for (let i of item) {
         delete obj.kv_pair[i];
       }
+if (isIQY) {
+  if (url.includes("/bottom_theme?")) {
+    // 爱奇艺 底部tab
+    if (obj?.cards?.length > 0) {
+      let card = obj.cards[0];
+      if (card?.items?.length > 0) {
+        // 29首页 31会员中心 34我的 35发现 184随刻视频
+        card.items = card.items.filter((i) => ["29", "31", "34"]?.includes(i?._id));
+        // 修复位置
+        for (let i = 0; i < card.items.length; i++) {
+          card.items[i].show_order = i + 1;
+        }
+      }
+    }
+  } else if (url.includes("/common_switch?")) {
+    // 爱奇艺 通用配置
+    if (obj?.content?.resource) {
+      const items = [
+        "activities",
+        "ai_guide", // ai指引
+        "cast_device_ad",
+        "flow_promotion", // 播放器 右上角免流按钮
+        "growth_award", // 播放器 会员成长积分
+        "ip_restriction_ad",
+        "member",
+        "ppc_feed_insert",
+        "second_floor_guide",
+        "speed_ad",
+        "vip_tips",
+        "vipgrowth_value", // 播放器 会员成长体系
+        "vr"
+      ];
+      for (let i of items) {
+        delete obj.content.resource[i];
+      }
+    }
+  } else if (url.includes("/control/")) {
+    // 爱奇艺 首页左上角天气图标
+    if (obj?.content?.weather) {
+      delete obj.content.weather;
+    }
+  } else if (url.includes("/getMyMenus?")) {
+    // 爱奇艺 我的页面
+    if (obj?.data?.length > 0) {
+      let newMenus = [];
+      for (let item of obj.data) {
+        if (["wd_liebiao_2", "wd_liebiao_3", "wd_liebiao_4"]?.includes(item?.statistic?.block)) {
+          // 精简列表
+          continue;
+        } else {
+          if (item?.menuList?.length > 0) {
+            let newLists = [];
+            for (let i of item.menuList) {
+              if (i?.menuType === 121) {
+                // 121有奖限时问卷
+                continue;
+              } else {
+                newLists.push(i);
+              }
+            }
+            item.menuList = newLists;
+            newMenus.push(item);
+          } else {
+            newMenus.push(item);
+          }
+        }
+      }
+      obj.data = newMenus;
+    }
+  } else if (url.includes("/home_top_menu?")) {
+    // 爱奇艺 顶部tab
+    if (obj?.cards?.length > 0) {
+      let card = obj.cards[0];
+      if (card?.items?.length > 0) {
+        // 1017直播 8196热点 4525518866820370中国梦
+        card.items = card.items.filter((i) => !["1017", "8196", "4525518866820370"]?.includes(i?._id));
+        for (let i = 0; i < card.items.length; i++) {
+          card.items[i].show_order = i + 1;
+        }
+      }
+    }
+  } else if (url.includes("/mixer?")) {
+    // 爱奇艺 开屏广告 播放广告
+    if (obj?.errorCode === 0) {
+      const items = ["adSlots", "splashLottieFile", "splashUiConfig"];
+      for (let i of items) {
+        delete obj[i];
+      }
+    }
+  } else if (url.includes("/search.video.iqiyi.com/")) {
+    // 爱奇艺 搜索框填充
+    if (obj?.cache_expired_sec) {
+      obj.cache_expired_sec = 1;
+    }
+    if (obj?.data) {
+      obj.data = [{ query: "搜索内容" }];
+    }
+    if (obj?.show_style?.roll_period) {
+      obj.show_style.roll_period = 1000;
+    }
+  } else if (url.includes("/views_category/")) {
+    // 爱奇艺 各菜单列表 剧集 电影 综艺 信息流
+    if (obj?.base?.statistics?.ad_str) {
+      delete obj.base.statistics.ad_str;
+    }
+    if (obj?.cards?.length > 0) {
+      let newCards = [];
+      for (let card of obj.cards) {
+        if (card?.blocks?.length > 0) {
+          let newItems = [];
+          for (let item of card.blocks) {
+            // block_321顶部轮播广告 block_415横版独占广告标题 block_416 横版独占视频广告
+            if (["block_321", "block_415", "block_416"]?.includes(item?.block_name)) {
+              continue;
+            } else if (item?.buttons?.[0]?.id === "ad") {
+              continue;
+            } else {
+              newItems.push(item);
+            }
+          }
+          card.blocks = newItems;
+          newCards.push(card);
+        } else {
+          newCards.push(card);
+        }
+      }
+      obj.cards = newCards;
+    }
+  } else if (url.includes("/views_comment/")) {
+    // 爱奇艺 播放页评论区
+    if (obj?.cards?.length > 0) {
+      // 评论资源位 无alias_name字段的为广告
+      obj.cards = obj.cards.filter(
+        (i) =>
+          i.hasOwnProperty("alias_name") &&
+          !["comment_resource_card", "comment_resource_convention_card"]?.includes(i?.alias_name)
+      );
+    }
+  } else if (url.includes("/views_home/")) {
+    // 爱奇艺 信息流样式1
+    if (obj?.base?.statistics?.ad_str) {
+      delete obj.base.statistics.ad_str;
+    }
+    if (obj?.cards?.length > 0) {
+      let newCards = [];
+      for (let card of obj.cards) {
+        // ad_mobile_flow信息流广告 ad_trueview信息流广告 focus顶部横版广告 qy_home_vip_opr_banner会员营销banner
+        if (["ad_mobile_flow", "ad_trueview", "focus", "qy_home_vip_opr_banner"]?.includes(card?.alias_name)) {
+          continue;
+        } else {
+          if (card?.top_banner?.l_blocks?.length > 0) {
+            // 模块右边文字按钮
+            for (let item of card.top_banner.l_blocks) {
+              if (item?.buttons?.length > 0) {
+                // 移除按钮 娱乐资源
+                delete item.buttons;
+              }
+            }
+            newCards.push(card);
+          } else {
+            newCards.push(card);
+          }
+        }
+      }
+      obj.cards = newCards;
+    }
+  } else if (url.includes("/views_plt/")) {
+    // 爱奇艺 播放详情页组件
+    if (obj?.kv_pair) {
+      // activity_tab活动标签页 cloud_cinema云影院卡片 vip_fixed_card会员优惠购买卡片
+      const items = ["activity_tab", "cloud_cinema", "vip_fixed_card"];
+      for (let i of items) {
+        delete obj.kv_pair[i];
+      }
     }
     if (obj?.cards?.length > 0) {
       obj.cards = obj.cards.filter(
         (i) =>
           ![
+            "bi_playlist", // 必播单 当下最热电影推荐
             // "cloud_cinema_detail_character", // 云影院演员列表
             // "cloud_cinema_detail_synopsis", // 云影院详情简介
             // "cloud_cinema_play_detail_tag", // 云影院详情标签
@@ -257,11 +432,12 @@ if (isIQY) {
         if (
           [
             "ad_mobile_flow", // 信息流广告
-            "hot_query_search_top_ad", //顶部广告
             "hot_query_bottom", // 底部图标
+            "hot_query_search_top_ad", //顶部广告
             "search_com_related_query", // 相关搜索
             "search_intent_detail_onesearch", // 为你推荐信息流
             "search_mid_text_ad", // 底部广告
+            "search_onebox_v2", // 搜索界面 赢年卡
             "search_small_card_ad", // 搜索短视频小图广告
             "search_topbanner_text", // 为你推荐标题
             "search_vip_banner" // vip营销
@@ -273,8 +449,14 @@ if (isIQY) {
           if (card?.blocks?.length > 0) {
             let newBlocks = [];
             for (let i of card.blocks) {
-              if (i?.hasOwnProperty("block_name")) {
+              if (i.hasOwnProperty("block_name")) {
                 newBlocks.push(i);
+              } else if (i.hasOwnProperty("block_type")) {
+                if (![861, 959]?.includes(i?.block_type)) {
+                  // 861搜索页精确搜索时 第一个自动播放的内容
+                  // 959广告
+                  newBlocks.push(i);
+                }
               }
             }
             card.blocks = newBlocks;
@@ -294,14 +476,14 @@ if (isIQY) {
     if (obj?.cards?.length > 0) {
       let newCards = [];
       for (let card of obj.cards) {
-        if (card?.hasOwnProperty("block_class")) {
+        if (card.hasOwnProperty("block_class")) {
           // 有block_class字段的为广告
           continue;
         } else {
           if (card?.blocks?.length > 0) {
             let newItems = [];
             for (let item of card.blocks) {
-              if (item?.hasOwnProperty("block_class")) {
+              if (item.hasOwnProperty("block_class")) {
                 // 有block_class字段的为广告
                 continue;
               } else {
